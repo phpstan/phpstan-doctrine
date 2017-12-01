c534c1d4a154df3cb3a0d834887ed04eade009f1
Stefan Siegl <stesie@brokenpipe.de>
2017-12-01 15:45:45 +0100

    Add dynamic return type extension for ObjectManager#merge

---
+++ src/Type/Doctrine/ObjectManagerMergeDynamicReturnTypeExtension.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Doctrine;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\Type;
+
+class ObjectManagerMergeDynamicReturnTypeExtension implements \PHPStan\Type\DynamicMethodReturnTypeExtension
+{
+
+	public function getClass(): string
+	{
+		return \Doctrine\Common\Persistence\ObjectManager::class;
+	}
+
+	public function isMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return $methodReflection->getName() === 'merge';
+	}
+
+	public function getTypeFromMethodCall(
+		MethodReflection $methodReflection,
+		MethodCall $methodCall,
+		Scope $scope
+	): Type
+	{
+		if (count($methodCall->args) === 0) {
+			return $methodReflection->getReturnType();
+		}
+
+		return $scope->getType($methodCall->args[0]->value);
+	}
+
+}
