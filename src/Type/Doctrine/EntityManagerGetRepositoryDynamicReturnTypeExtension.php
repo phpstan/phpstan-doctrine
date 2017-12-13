f42f93f75ada7f577099e0e157faa8f3f3a0721c
Maciej Malarz <malarzm@gmail.com>
2017-12-13 13:24:27 +0100

    Resolve custom repository classes

--- src/Type/Doctrine/EntityManagerGetRepositoryDynamicReturnTypeExtension.php
+++ src/Type/Doctrine/EntityManagerGetRepositoryDynamicReturnTypeExtension.php
@@ -2,23 +2,27 @@
 
 namespace PHPStan\Type\Doctrine;
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Persistence\Mapping\MappingException;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
+use PHPStan\DoctrineClassMetadataProvider;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Type\Type;
 
 class EntityManagerGetRepositoryDynamicReturnTypeExtension implements \PHPStan\Type\DynamicMethodReturnTypeExtension
 {
 
-	/**
-	 * @var string
-	 */
-	private $repositoryClass;
+    /**
+     * @var DoctrineClassMetadataProvider
+     */
+    private $metadataProvider;
 
-	public function __construct(string $repositoryClass)
+    public function __construct(DoctrineClassMetadataProvider $metadataProvider)
 	{
-		$this->repositoryClass = $repositoryClass;
-	}
+        $this->metadataProvider = $metadataProvider;
+        AnnotationRegistry::registerLoader('class_exists');
+    }
 
 	public function getClass(): string
 	{
@@ -58,7 +62,12 @@ class EntityManagerGetRepositoryDynamicReturnTypeExtension implements \PHPStan\T
 			$class = $scope->getClassReflection()->getName();
 		}
 
-		return new EntityRepositoryType($class, $this->repositoryClass);
+        try {
+            $metadata = $this->metadataProvider->getMetadataFor($class);
+            return new EntityRepositoryType($class, $metadata->customRepositoryClassName ?: $this->metadataProvider->getBaseRepositoryClass());
+        } catch (MappingException $e) {
+		    return new EntityRepositoryType($class, $this->metadataProvider->getBaseRepositoryClass());
+        }
 	}
 
 }
