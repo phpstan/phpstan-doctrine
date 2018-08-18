5c58d993b1cd49d0eb40e1cfa203bccb6551ec8e
Luís Cobucci <lcobucci@gmail.com>
2018-08-18 15:03:45 +0200

    Add integration test for entity manager dynamic return

---
+++ tests/DoctrineIntegration/ORM/EntityManagerIntegrationTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\DoctrineIntegration\ORM;
+
+use PHPStan\Testing\LevelsTestCase;
+
+final class EntityManagerIntegrationTest extends LevelsTestCase
+{
+
+	/**
+	 * @return string[][]
+	 */
+	public function dataTopics(): array
+	{
+		return [['entityManagerDynamicReturn']];
+	}
+
+	public function getDataPath(): string
+	{
+		return __DIR__ . '/data';
+	}
+
+	public function getPhpStanExecutablePath(): string
+	{
+		return __DIR__ . '/../../../vendor/bin/phpstan';
+	}
+
+	public function getPhpStanConfigPath(): ?string
+	{
+		return __DIR__ . '/phpstan.neon';
+	}
+
+}
---
+++ tests/DoctrineIntegration/ORM/data/entityManagerDynamicReturn.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\DoctrineIntegration\ORM\EntityManagerDynamicReturn;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping as ORM;
+use RuntimeException;
+
+class Example
+{
+	/**
+	 * @var EntityManagerInterface
+	 */
+	private $entityManager;
+
+	public function __construct(EntityManagerInterface $entityManager)
+	{
+		$this->entityManager = $entityManager;
+	}
+
+	public function findDynamicType(): void
+	{
+		$test = $this->entityManager->find(MyEntity::class, 1);
+
+		if ($test === null) {
+			throw new RuntimeException('Sorry, but no...');
+		}
+
+		$test->doSomething();
+	}
+
+	public function getReferenceDynamicType(): void
+	{
+		$test = $this->entityManager->getReference(MyEntity::class, 1);
+		$test->doSomething();
+	}
+
+	public function getPartialReferenceDynamicType(): void
+	{
+		$test = $this->entityManager->getPartialReference(MyEntity::class, 1);
+		$test->doSomething();
+	}
+}
+
+/**
+ * @ORM\Entity()
+ */
+class MyEntity
+{
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 *
+	 * @var int
+	 */
+	private $id;
+
+	public function doSomething(): void
+	{
+	}
+}
