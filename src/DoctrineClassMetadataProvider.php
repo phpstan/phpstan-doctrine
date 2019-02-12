df0181315b2570652708785223e1937de09a9008
Fred Cox <mcfedr@gmail.com>
2019-02-12 12:14:09 +0200

    Fixed

--- src/DoctrineClassMetadataProvider.php
+++ src/DoctrineClassMetadataProvider.php
@@ -3,9 +3,11 @@
 namespace PHPStan;
 
 use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\Common\EventManager;
 use Doctrine\Common\Persistence\Mapping\Driver\MappingDriver;
 use Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain;
+use Doctrine\Common\Persistence\Mapping\MappingException;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping;
@@ -14,68 +16,78 @@ use Doctrine\ORM\Proxy\ProxyFactory;
 class DoctrineClassMetadataProvider
 {
 
-    /**
-     * @var EntityManager
-     */
-    private $em;
+	/** @var ?EntityManager */
+	private $em;
 
-    /**
-     * @var string
-     */
-    private $repositoryClass;
+	/** @var string */
+	private $repositoryClass;
 
-    public function __construct(string $repositoryClass, array $mapping)
-    {
-        $configuration = new Configuration();
-        $configuration->setDefaultRepositoryClassName($repositoryClass);
-        $configuration->setMetadataDriverImpl($this->setupMappingDriver($mapping));
-        $configuration->setProxyDir('/dev/null');
-        $configuration->setProxyNamespace('__DP__');
-        $configuration->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_EVAL);
-        $evm = new EventManager();
-        $this->em = EntityManager::create(
-            \Doctrine\DBAL\DriverManager::getConnection(['host' => '/:memory:', 'driver' => 'pdo_sqlite'], $configuration, $evm),
-            $configuration,
-            $evm
-        );
-        $this->repositoryClass = $repositoryClass;
-    }
+	/**
+	 * @param string $repositoryClass
+	 * @param mixed[] $mapping
+	 */
+	public function __construct(string $repositoryClass, array $mapping)
+	{
+		if (count($mapping) > 0) {
+			$configuration = new Configuration();
+			$configuration->setDefaultRepositoryClassName($repositoryClass);
+			$configuration->setMetadataDriverImpl($this->setupMappingDriver($mapping));
+			$configuration->setProxyDir('/dev/null');
+			$configuration->setProxyNamespace('__DP__');
+			$configuration->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_EVAL);
+			$evm = new EventManager();
+			$this->em = EntityManager::create(
+				\Doctrine\DBAL\DriverManager::getConnection(['host' => '/:memory:', 'driver' => 'pdo_sqlite'], $configuration, $evm),
+				$configuration,
+				$evm
+			);
+		}
+		$this->repositoryClass = $repositoryClass;
+	}
 
-    private function setupMappingDriver(array $mapping): MappingDriver
-    {
-        $driver = new MappingDriverChain();
-        foreach ($mapping as $namespace => $config) {
-            switch ($config['type']) {
-                case 'annotation':
-                    $nested = new Mapping\Driver\AnnotationDriver(new AnnotationReader(), $config['paths']);
-                    break;
-                case 'yml':
-                case 'yaml':
-                    $nested = new Mapping\Driver\YamlDriver($config['paths']);
-                    break;
-                case 'xml':
-                    $nested = new Mapping\Driver\XmlDriver($config['paths']);
-                    break;
-                default:
-                    throw new \InvalidArgumentException('Unknown mapping type: ' . $config['type']);
-            }
-            $driver->addDriver($nested, $namespace);
-        }
-        return $driver;
-    }
+	/**
+	 * @param mixed[] $mapping
+	 */
+	private function setupMappingDriver(array $mapping): MappingDriver
+	{
+		$driver = new MappingDriverChain();
+		foreach ($mapping as $namespace => $config) {
+			switch ($config['type']) {
+				case 'annotation':
+					AnnotationRegistry::registerUniqueLoader('class_exists');
+					$nested = new Mapping\Driver\AnnotationDriver(new AnnotationReader(), $config['paths']);
+					break;
+				case 'yml':
+				case 'yaml':
+					$nested = new Mapping\Driver\YamlDriver($config['paths']);
+					break;
+				case 'xml':
+					$nested = new Mapping\Driver\XmlDriver($config['paths']);
+					break;
+				default:
+					throw new \InvalidArgumentException('Unknown mapping type: ' . $config['type']);
+			}
+			$driver->addDriver($nested, $namespace);
+		}
+		return $driver;
+	}
 
-    public function getBaseRepositoryClass(): string
-    {
-        return $this->repositoryClass;
-    }
+	public function getBaseRepositoryClass(): string
+	{
+		return $this->repositoryClass;
+	}
 
-    /**
-     * @throws \Doctrine\Common\Persistence\Mapping\MappingException
-     * @throws \ReflectionException
-     */
-    public function getMetadataFor(string $className): Mapping\ClassMetadataInfo
-    {
-        return $this->em->getClassMetadata($className);
-    }
-    
+	public function getRepositoryClass(string $className): string
+	{
+		if ($this->em === null) {
+			return $this->getBaseRepositoryClass();
+		}
+
+		try {
+			return $this->em->getClassMetadata($className)->customRepositoryClassName ?: $this->getBaseRepositoryClass();
+		} catch (MappingException $e) {
+			return $this->getBaseRepositoryClass();
+		}
+	}
+
 }
--- src/Type/Doctrine/ObjectManagerGetRepositoryDynamicReturnTypeExtension.php
+++ src/Type/Doctrine/ObjectManagerGetRepositoryDynamicReturnTypeExtension.php
@@ -2,8 +2,6 @@
 
 namespace PHPStan\Type\Doctrine;
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\Common\Persistence\Mapping\MappingException;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\DoctrineClassMetadataProvider;
@@ -16,19 +14,13 @@ use PHPStan\Type\Type;
 class ObjectManagerGetRepositoryDynamicReturnTypeExtension implements \PHPStan\Type\DynamicMethodReturnTypeExtension
 {
 
-	/** @var string */
-	private $repositoryClass;
+	/** @var DoctrineClassMetadataProvider */
+	private $metadataProvider;
 
-    /**
-     * @var DoctrineClassMetadataProvider
-     */
-    private $metadataProvider;
-
-    public function __construct(DoctrineClassMetadataProvider $metadataProvider)
+	public function __construct(DoctrineClassMetadataProvider $metadataProvider)
 	{
-        $this->metadataProvider = $metadataProvider;
-        AnnotationRegistry::registerLoader('class_exists');
-    }
+		$this->metadataProvider = $metadataProvider;
+	}
 
 	public function getClass(): string
 	{
@@ -56,28 +48,7 @@ class ObjectManagerGetRepositoryDynamicReturnTypeExtension implements \PHPStan\T
 			return new MixedType();
 		}
 
-		return new ObjectRepositoryType($argType->getValue(), $this->repositoryClass);
-
-		$class = $arg->class;
-		if (!($class instanceof \PhpParser\Node\Name)) {
-			return $methodReflection->getReturnType();
-		}
-
-		$class = (string) $class;
-		if ($class === 'static') {
-			return $methodReflection->getReturnType();
-		}
-
-		if ($class === 'self') {
-			$class = $scope->getClassReflection()->getName();
-		}
-
-        try {
-            $metadata = $this->metadataProvider->getMetadataFor($class);
-            return new EntityRepositoryType($class, $metadata->customRepositoryClassName ?: $this->metadataProvider->getBaseRepositoryClass());
-        } catch (MappingException $e) {
-		    return new EntityRepositoryType($class, $this->metadataProvider->getBaseRepositoryClass());
-        }
+		return new ObjectRepositoryType($argType->getValue(), $this->metadataProvider->getRepositoryClass($argType->getValue()));
 	}
 
 }
