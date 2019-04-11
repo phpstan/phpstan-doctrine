45b9a898d5dfa11df4421901c0b6df5291b1fd11
Ondrej Mirtes <ondrej@mirtes.cz>
2019-04-11 17:02:49 +0200

    Simplified code

--- src/Type/Doctrine/QueryBuilder/QueryBuilderType.php
+++ src/Type/Doctrine/QueryBuilder/QueryBuilderType.php
@@ -3,38 +3,13 @@
 namespace PHPStan\Type\Doctrine\QueryBuilder;
 
 use PhpParser\Node\Expr\MethodCall;
-use PHPStan\TrinaryLogic;
 use PHPStan\Type\ObjectType;
-use PHPStan\Type\Type;
 
 class QueryBuilderType extends ObjectType
 {
 
 	/** @var MethodCall[] */
 	private $methodCalls = [];
-
-	public function __construct(string $queryBuilderClass)
-	{
-		parent::__construct($queryBuilderClass);
-	}
-
-	public function isSuperTypeOf(Type $type): TrinaryLogic
-	{
-		if ($type instanceof self) {
-			if (count($this->methodCalls) > count($type->methodCalls)) {
-				return TrinaryLogic::createMaybe();
-			}
-
-			return TrinaryLogic::createYes();
-		}
-
-		// todo taky musí fungovat type-specifying extension, tedy intersect
-		// intersect s více metodami musí vyhrát
-		// kombinovat pouze QB vycházející ze stejného řádku
-		// kombinovat pouze QB typy které se nevětví - udržovat si nějaký identifikátor pokaždé?
-
-		return parent::isSuperTypeOf($type);
-	}
 
 	/**
 	 * @return MethodCall[]
