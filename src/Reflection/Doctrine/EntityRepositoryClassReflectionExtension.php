98a70724aaa360657d62903434d0193ec4b09b8b
Ondrej Mirtes <ondrej@mirtes.cz>
2019-04-14 17:29:07 +0200

    Fixed error if Doctrine\ORM\EntityRepository class is not defined

--- src/Reflection/Doctrine/EntityRepositoryClassReflectionExtension.php
+++ src/Reflection/Doctrine/EntityRepositoryClassReflectionExtension.php
@@ -2,21 +2,40 @@
 
 namespace PHPStan\Reflection\Doctrine;
 
+use PHPStan\Broker\Broker;
+use PHPStan\Reflection\BrokerAwareExtension;
 use PHPStan\Reflection\Dummy\DummyMethodReflection;
 
-class EntityRepositoryClassReflectionExtension implements \PHPStan\Reflection\MethodsClassReflectionExtension
+class EntityRepositoryClassReflectionExtension implements \PHPStan\Reflection\MethodsClassReflectionExtension, BrokerAwareExtension
 {
+
+	/** @var Broker */
+	private $broker;
+
+	public function setBroker(Broker $broker): void
+	{
+		$this->broker = $broker;
+	}
 
 	public function hasMethod(\PHPStan\Reflection\ClassReflection $classReflection, string $methodName): bool
 	{
-		return (
-			$classReflection->getName() === 'Doctrine\ORM\EntityRepository'
-			|| $classReflection->isSubclassOf('Doctrine\ORM\EntityRepository')
-		) && (
-			strpos($methodName, 'findBy') === 0
-			|| strpos($methodName, 'findOneBy') === 0
-			|| strpos($methodName, 'countBy') === 0
-		);
+		if (
+			strpos($methodName, 'findBy') !== 0
+			&& strpos($methodName, 'findOneBy') !== 0
+			&& strpos($methodName, 'countBy') !== 0
+		) {
+			return false;
+		}
+
+		if ($classReflection->getName() === 'Doctrine\ORM\EntityRepository') {
+			return true;
+		}
+
+		if (!$this->broker->hasClass('Doctrine\ORM\EntityRepository')) {
+			return false;
+		}
+
+		return $classReflection->isSubclassOf('Doctrine\ORM\EntityRepository');
 	}
 
 	public function getMethod(\PHPStan\Reflection\ClassReflection $classReflection, string $methodName): \PHPStan\Reflection\MethodReflection
