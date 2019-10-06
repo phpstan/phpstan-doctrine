15bf6ca691a55c808e44ba847d4d8d7c92c67e5b
Lukáš Unger <looky.msc@gmail.com>
2019-10-06 12:20:21 +0200

    Added tests for ToMany relations

--- tests/Rules/Doctrine/ORM/EntityRelationRuleTest.php
+++ tests/Rules/Doctrine/ORM/EntityRelationRuleTest.php
@@ -29,43 +29,85 @@ class EntityRelationRuleTest extends RuleTestCase
 	{
 		yield 'nice entity' => [__DIR__ . '/data/EntityWithRelations.php', []];
 
-		yield 'one to one' => [__DIR__ . '/data/EntityWithBrokenOneToOneRelations.php', [
+		yield 'one to one' => [__DIR__ . '/data/EntityWithBrokenOneToOneRelations.php',
 			[
-				'Property can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but database expects PHPStan\Rules\Doctrine\ORM\AnotherEntity.',
-				31,
-			],
-			[
-				'Database can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but property expects PHPStan\Rules\Doctrine\ORM\AnotherEntity.',
-				37,
-			],
-			[
-				'Database can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but property expects PHPStan\Rules\Doctrine\ORM\MyEntity|null.',
-				50,
-			],
-			[
-				'Property can contain PHPStan\Rules\Doctrine\ORM\MyEntity|null but database expects PHPStan\Rules\Doctrine\ORM\AnotherEntity|null.',
-				50,
-			]
-		]];
+				[
+					'Property can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but database expects PHPStan\Rules\Doctrine\ORM\AnotherEntity.',
+					31,
+				],
+				[
+					'Database can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but property expects PHPStan\Rules\Doctrine\ORM\AnotherEntity.',
+					37,
+				],
+				[
+					'Database can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but property expects PHPStan\Rules\Doctrine\ORM\MyEntity|null.',
+					50,
+				],
+				[
+					'Property can contain PHPStan\Rules\Doctrine\ORM\MyEntity|null but database expects PHPStan\Rules\Doctrine\ORM\AnotherEntity|null.',
+					50,
+				],
+			]];
 
-		yield 'many to one' => [__DIR__ . '/data/EntityWithBrokenManyToOneRelations.php', [
+		yield 'many to one' => [__DIR__ . '/data/EntityWithBrokenManyToOneRelations.php',
 			[
-				'Property can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but database expects PHPStan\Rules\Doctrine\ORM\AnotherEntity.',
-				31,
-			],
+				[
+					'Property can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but database expects PHPStan\Rules\Doctrine\ORM\AnotherEntity.',
+					31,
+				],
+				[
+					'Database can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but property expects PHPStan\Rules\Doctrine\ORM\AnotherEntity.',
+					37,
+				],
+				[
+					'Database can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but property expects PHPStan\Rules\Doctrine\ORM\MyEntity|null.',
+					50,
+				],
+				[
+					'Property can contain PHPStan\Rules\Doctrine\ORM\MyEntity|null but database expects PHPStan\Rules\Doctrine\ORM\AnotherEntity|null.',
+					50,
+				],
+			]];
+
+		yield 'one to many' => [__DIR__ . '/data/EntityWithBrokenOneToManyRelations.php',
 			[
-				'Database can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but property expects PHPStan\Rules\Doctrine\ORM\AnotherEntity.',
-				37,
-			],
+				[
+					'Property can contain iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity> but database expects Doctrine\Common\Collections\Collection&iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity>.',
+					24,
+				],
+				[
+					'Property can contain Doctrine\Common\Collections\Collection but database expects Doctrine\Common\Collections\Collection&iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity>.',
+					30,
+				],
+				[
+					'Database can contain Doctrine\Common\Collections\Collection&iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity> but property expects array<PHPStan\Rules\Doctrine\ORM\AnotherEntity>.',
+					36,
+				],
+				[
+					'Property can contain array<PHPStan\Rules\Doctrine\ORM\AnotherEntity> but database expects Doctrine\Common\Collections\Collection&iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity>.',
+					36,
+				],
+			]];
+
+		yield 'many to many' => [__DIR__ . '/data/EntityWithBrokenManyToManyRelations.php',
 			[
-				'Database can contain PHPStan\Rules\Doctrine\ORM\AnotherEntity|null but property expects PHPStan\Rules\Doctrine\ORM\MyEntity|null.',
-				50,
-			],
-			[
-				'Property can contain PHPStan\Rules\Doctrine\ORM\MyEntity|null but database expects PHPStan\Rules\Doctrine\ORM\AnotherEntity|null.',
-				50,
-			]
-		]];
+				[
+					'Property can contain iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity> but database expects Doctrine\Common\Collections\Collection&iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity>.',
+					24,
+				],
+				[
+					'Property can contain Doctrine\Common\Collections\Collection but database expects Doctrine\Common\Collections\Collection&iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity>.',
+					30,
+				],
+				[
+					'Database can contain Doctrine\Common\Collections\Collection&iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity> but property expects array<PHPStan\Rules\Doctrine\ORM\AnotherEntity>.',
+					36,
+				],
+				[
+					'Property can contain array<PHPStan\Rules\Doctrine\ORM\AnotherEntity> but database expects Doctrine\Common\Collections\Collection&iterable<PHPStan\Rules\Doctrine\ORM\AnotherEntity>.',
+					36,
+				],
+			]];
 	}
 
 }
