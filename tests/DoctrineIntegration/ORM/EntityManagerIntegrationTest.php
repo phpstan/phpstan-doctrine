586f7087e1d607cfa96b7bca4a712f7e4de42a3d
Martin Hujer <mail@martinhujer.cz>
2019-11-30 19:03:05 +0100

    tests: use PHPStan binary directly
    another attempt to fix 73c8ebd86343fbfa4cc0477f798909f430332ac4

--- tests/DoctrineIntegration/ORM/EntityManagerIntegrationTest.php
+++ tests/DoctrineIntegration/ORM/EntityManagerIntegrationTest.php
@@ -28,7 +28,7 @@ final class EntityManagerIntegrationTest extends LevelsTestCase
 
 	public function getPhpStanExecutablePath(): string
 	{
-		return __DIR__ . '/../../../vendor/bin/phpstan';
+		return __DIR__ . '/../../../vendor/phpstan/phpstan/phpstan';
 	}
 
 	public function getPhpStanConfigPath(): string
