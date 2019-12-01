cff9f4e16318640da04537bb28bd774d13b60a6b
Martin Hujer <mail@martinhujer.cz>
2019-12-01 10:58:36 +0100

    fixup! Ramsey UUID TypeDescriptor

--- src/Type/Doctrine/Descriptors/Ramsey/UuidTypeDescriptor.php
+++ src/Type/Doctrine/Descriptors/Ramsey/UuidTypeDescriptor.php
@@ -5,15 +5,37 @@ namespace PHPStan\Type\Doctrine\Descriptors\Ramsey;
 use PHPStan\Type\Doctrine\Descriptors\DoctrineTypeDescriptor;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
-use Ramsey\Uuid\Doctrine\UuidType;
 use Ramsey\Uuid\UuidInterface;
 
 class UuidTypeDescriptor implements DoctrineTypeDescriptor
 {
 
+	private const SUPPORTED_UUID_TYPES = [
+		\Ramsey\Uuid\Doctrine\UuidType::class,
+		\Ramsey\Uuid\Doctrine\UuidBinaryType::class,
+		\Ramsey\Uuid\Doctrine\UuidBinaryOrderedTimeType::class,
+	];
+
+	/** @var string */
+	private $uuidTypeName;
+
+	public function __construct(
+		string $uuidTypeName
+	)
+	{
+		if (!in_array($uuidTypeName, self::SUPPORTED_UUID_TYPES, true)) {
+			throw new \PHPStan\ShouldNotHappenException(sprintf(
+				'Unexpected UUID column type "%s" provided',
+				$uuidTypeName
+			));
+		}
+
+		$this->uuidTypeName = $uuidTypeName;
+	}
+
 	public function getType(): string
 	{
-		return UuidType::class;
+		return $this->uuidTypeName;
 	}
 
 	public function getWritableToPropertyType(): Type
