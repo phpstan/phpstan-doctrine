196beae8039e8a9eff3050eea466d2bd1dea4c7e
Ondrej Mirtes <ondrej@mirtes.cz>
2022-01-15 15:15:56 +0100

    Fix

--- tests/Rules/Doctrine/ORM/EntityMappingExceptionRuleTest.php
+++ tests/Rules/Doctrine/ORM/EntityMappingExceptionRuleTest.php
@@ -5,6 +5,7 @@ namespace PHPStan\Rules\Doctrine\ORM;
 use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
 use PHPStan\Type\Doctrine\ObjectMetadataResolver;
+use const PHP_VERSION_ID;
 
 /**
  * @extends RuleTestCase<EntityMappingExceptionRule>
@@ -21,11 +22,17 @@ class EntityMappingExceptionRuleTest extends RuleTestCase
 
 	public function testValidEntity(): void
 	{
+		if (PHP_VERSION_ID < 80100) {
+			self::markTestSkipped('Test requires PHP 8.1.');
+		}
 		$this->analyse([__DIR__ . '/data-attributes/enum-type.php'], []);
 	}
 
 	public function testInvalidEntity(): void
 	{
+		if (PHP_VERSION_ID < 80100) {
+			self::markTestSkipped('Test requires PHP 8.1.');
+		}
 		$this->analyse([__DIR__ . '/data-attributes/enum-type-without-pk.php'], [
 			[
 				'No identifier/primary key specified for Entity "PHPStan\Rules\Doctrine\ORMAttributes\FooWithoutPK". Every Entity must have an identifier/primary key.',
