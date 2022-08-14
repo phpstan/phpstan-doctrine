5afea15a536b60ff4d9cd7c6a2d010c544c00641
Jeroen Thora <jeroenthora@gmail.com>
2022-08-14 11:10:18 +0200

    Add ReadWritePropertiesExtension for Gedmo annotations/attributes

---
+++ tests/Rules/Properties/MissingGedmoPropertyAssignRuleTest.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Properties;
+
+use Iterator;
+use PHPStan\Rules\DeadCode\UnusedPrivatePropertyRule;
+use PHPStan\Rules\Gedmo\PropertiesExtension;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use PHPStan\Type\Doctrine\ObjectMetadataResolver;
+use const PHP_VERSION_ID;
+
+/**
+ * @extends RuleTestCase<UnusedPrivatePropertyRule>
+ */
+class MissingGedmoPropertyAssignRuleTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return self::getContainer()->getByType(UnusedPrivatePropertyRule::class);
+	}
+
+	protected function getReadWritePropertiesExtensions(): array
+	{
+		return [
+			new PropertiesExtension(new ObjectMetadataResolver(__DIR__ . '/entity-manager.php')),
+		];
+	}
+
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [__DIR__ . '/../../../extension.neon'];
+	}
+
+	/**
+	 * @dataProvider ruleProvider
+	 * @param mixed[] $expectedErrors
+	 */
+	public function testRule(string $file, array $expectedErrors): void
+	{
+		if (PHP_VERSION_ID < 80100) {
+			self::markTestSkipped('Test requires PHP 8.1.');
+		}
+
+		$this->analyse([$file], $expectedErrors);
+	}
+
+	/**
+	 * @return Iterator<mixed[]>
+	 */
+	public function ruleProvider(): Iterator
+	{
+		yield 'entity with gedmo attributes' => [
+			__DIR__ . '/data/gedmo-property-assign.php',
+			[
+				// No errors expected
+			],
+		];
+
+		yield 'non-entity with gedmo attributes' => [
+			__DIR__ . '/data/gedmo-property-assign-non-entity.php',
+			[
+				[
+					'Property MissingGedmoWrittenPropertyAssign\NonEntityWithAGemdoLocaleField::$locale is unused.',
+					10,
+					'See: https://phpstan.org/developing-extensions/always-read-written-properties',
+				],
+			],
+		];
+	}
+
+}
