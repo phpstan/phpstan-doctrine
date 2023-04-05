61a4dc1e87a41f2d2f7bcde34866e1e788334c3a
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-04-05 21:04:39 +0200

    Only use benevolent when where clause is used

--- src/Type/Doctrine/Query/QueryResultTypeWalker.php
+++ src/Type/Doctrine/Query/QueryResultTypeWalker.php
@@ -107,6 +107,9 @@ class QueryResultTypeWalker extends SqlWalker
 	/** @var bool */
 	private $hasGroupByClause;
 
+	/** @var bool */
+	private $hasCondition;
+
 	/**
 	 * @param Query<mixed> $query
 	 */
@@ -135,6 +138,7 @@ class QueryResultTypeWalker extends SqlWalker
 		$this->nullableQueryComponents = [];
 		$this->hasAggregateFunction = false;
 		$this->hasGroupByClause = false;
+		$this->hasCondition = false;
 
 		// The object is instantiated by Doctrine\ORM\Query\Parser, so receiving
 		// dependencies through the constructor is not an option. Instead, we
@@ -590,6 +594,8 @@ class QueryResultTypeWalker extends SqlWalker
 	 */
 	public function walkHavingClause($havingClause)
 	{
+		$this->hasCondition = true;
+
 		return $this->marshalType(new MixedType());
 	}
 
@@ -994,6 +1000,8 @@ class QueryResultTypeWalker extends SqlWalker
 	 */
 	public function walkWhereClause($whereClause)
 	{
+		$this->hasCondition = true;
+
 		return $this->marshalType(new MixedType());
 	}
 
@@ -1281,7 +1289,10 @@ class QueryResultTypeWalker extends SqlWalker
 	 */
 	private function addScalar($alias, Type $type): void
 	{
-		if ($type instanceof UnionType) {
+		// Since we don't check the condition inside the WHERE or HAVING
+		// conditions, we cannot be sure all the union types are correct.
+		// For exemple, a condition `WHERE foo.bar IS NOT NULL` could be added.
+		if ($this->hasCondition && $type instanceof UnionType) {
 			$type = TypeUtils::toBenevolentUnion($type);
 		}
 
