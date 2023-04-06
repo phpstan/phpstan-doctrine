7382b34a2f795589d6f476bf67af9a7e7e528309
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-04-06 10:27:06 +0200

    Fix check for where condition

--- src/Type/Doctrine/Query/QueryResultTypeWalker.php
+++ src/Type/Doctrine/Query/QueryResultTypeWalker.php
@@ -107,7 +107,7 @@ class QueryResultTypeWalker extends SqlWalker
 	private $hasGroupByClause;
 
 	/** @var bool */
-	private $hasCondition;
+	private $hasWhereClause;
 
 	/**
 	 * @param Query<mixed> $query
@@ -137,7 +137,7 @@ class QueryResultTypeWalker extends SqlWalker
 		$this->nullableQueryComponents = [];
 		$this->hasAggregateFunction = false;
 		$this->hasGroupByClause = false;
-		$this->hasCondition = false;
+		$this->hasWhereClause = false;
 
 		// The object is instantiated by Doctrine\ORM\Query\Parser, so receiving
 		// dependencies through the constructor is not an option. Instead, we
@@ -180,6 +180,7 @@ class QueryResultTypeWalker extends SqlWalker
 		$this->typeBuilder->setSelectQuery();
 		$this->hasAggregateFunction = $this->hasAggregateFunction($AST);
 		$this->hasGroupByClause = $AST->groupByClause !== null;
+		$this->hasWhereClause = $AST->whereClause !== null;
 
 		$this->walkFromClause($AST->fromClause);
 
@@ -593,8 +594,6 @@ class QueryResultTypeWalker extends SqlWalker
 	 */
 	public function walkHavingClause($havingClause)
 	{
-		$this->hasCondition = true;
-
 		return $this->marshalType(new MixedType());
 	}
 
@@ -1010,8 +1009,6 @@ class QueryResultTypeWalker extends SqlWalker
 	 */
 	public function walkWhereClause($whereClause)
 	{
-		$this->hasCondition = true;
-
 		return $this->marshalType(new MixedType());
 	}
 
@@ -1299,10 +1296,10 @@ class QueryResultTypeWalker extends SqlWalker
 	 */
 	private function addScalar($alias, Type $type): void
 	{
-		// Since we don't check the condition inside the WHERE or HAVING
+		// Since we don't check the condition inside the WHERE
 		// conditions, we cannot be sure all the union types are correct.
 		// For exemple, a condition `WHERE foo.bar IS NOT NULL` could be added.
-		if ($this->hasCondition && $type instanceof UnionType) {
+		if ($this->hasWhereClause && $type instanceof UnionType) {
 			$type = TypeUtils::toBenevolentUnion($type);
 		}
 
