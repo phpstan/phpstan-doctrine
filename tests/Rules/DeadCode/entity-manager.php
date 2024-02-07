1860a9cca5400e6ef4db4557e7500e7313faf831
Ondrej Mirtes <ondrej@mirtes.cz>
2024-02-07 23:16:06 +0100

    Update cache in tests to use PSR-6

--- tests/Rules/DeadCode/entity-manager.php
+++ tests/Rules/DeadCode/entity-manager.php
@@ -1,18 +1,17 @@
 <?php declare(strict_types = 1);
 
+use Cache\Adapter\PHPArray\ArrayCachePool;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\ORM\Mapping\Driver\AttributeDriver;
 use Doctrine\Persistence\Mapping\Driver\MappingDriverChain;
-use Symfony\Component\Cache\Adapter\ArrayAdapter;
-use Symfony\Component\Cache\DoctrineProvider;
 
 $config = new Configuration();
 $config->setProxyDir(__DIR__);
 $config->setProxyNamespace('PHPstan\Doctrine\OrmProxies');
-$config->setMetadataCacheImpl(new DoctrineProvider(new ArrayAdapter()));
+$config->setMetadataCache(new ArrayCachePool());
 
 $metadataDriver = new MappingDriverChain();
 $metadataDriver->addDriver(new AnnotationDriver(
