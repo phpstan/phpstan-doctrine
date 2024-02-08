8cdab4b07174a3f8f17a0df23d7dbf4bbc91ad07
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-02-08 15:39:26 +0100

    Wip

--- src/Type/Doctrine/Descriptors/BigIntType.php
+++ src/Type/Doctrine/Descriptors/BigIntType.php
@@ -2,6 +2,8 @@
 
 namespace PHPStan\Type\Doctrine\Descriptors;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use PHPStan\Type\Accessory\AccessoryNumericStringType;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\StringType;
@@ -18,17 +20,46 @@ class BigIntType implements DoctrineTypeDescriptor
 
 	public function getWritableToPropertyType(): Type
 	{
-		return TypeCombinator::intersect(new StringType(), new AccessoryNumericStringType());
+		if (!$this->hasDoctrineDbal4()) {
+			return TypeCombinator::intersect(new StringType(), new AccessoryNumericStringType());
+		}
+
+		if ($this->usePHP64Bit()) { // TODO: Add `|| Bigint is not unsigned`
+			return new IntegerType();
+		}
+
+		return TypeCombinator::union(
+			new IntegerType(),
+			TypeCombinator::intersect(new StringType(), new AccessoryNumericStringType()),
+		);
 	}
 
 	public function getWritableToDatabaseType(): Type
 	{
+		if (!$this->hasDoctrineDbal4()) {
+			return TypeCombinator::union(new StringType(), new IntegerType());
+		}
+
+		if ($this->usePHP64Bit()) { // TODO: Add `|| Bigint is not unsigned`
+			return new IntegerType();
+		}
+
 		return TypeCombinator::union(new StringType(), new IntegerType());
 	}
 
 	public function getDatabaseInternalType(): Type
 	{
 		return new IntegerType();
+	}
+
+	public function hasDoctrineDbal4(): bool
+	{
+		return InstalledVersions::satisfies(new VersionParser(), 'doctrine/dbal', '>= 4');
+	}
+
+	public function usePHP64Bit(): bool
+	{
+		return PHP_INT_SIZE === 8;
 	}
 
 }
