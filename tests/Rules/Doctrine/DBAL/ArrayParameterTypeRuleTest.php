e5cab7734e69764b80ec077b3228fe226c81d2fd
Gert de Pagter <gert.de.pagter@yoursurprise.com>
2024-02-09 15:29:17 +0100

    Deal with older dbal versiosn

--- tests/Rules/Doctrine/DBAL/ArrayParameterTypeRuleTest.php
+++ tests/Rules/Doctrine/DBAL/ArrayParameterTypeRuleTest.php
@@ -2,6 +2,8 @@
 
 namespace PHPStan\Rules\Doctrine\DBAL;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
 
@@ -11,8 +13,44 @@ use PHPStan\Testing\RuleTestCase;
 final class ArrayParameterTypeRuleTest extends RuleTestCase
 {
 
+	public function testRuleOlderDbal(): void
+	{
+		if(InstalledVersions::satisfies(
+			new VersionParser(),
+			'doctrine/dbal',
+			'^3.6 || ^4.0'
+		)) {
+			self::markTestSkipped('Test requires dbal 2.');
+		}
+		$this->analyse([__DIR__ . '/data/connection_dbal2.php'], [
+			[
+				'Parameter at 0 is an array, but is not hinted as such to doctrine.',
+				10,
+			],
+			[
+				"Parameter at 'a' is an array, but is not hinted as such to doctrine.",
+				19,
+			],
+			[
+				"Parameter at 'a' is an array, but is not hinted as such to doctrine.",
+				28,
+			],
+			[
+				"Parameter at 'a' is an array, but is not hinted as such to doctrine.",
+				39,
+			],
+		]);
+	}
+
 	public function testRule(): void
 	{
+		if(InstalledVersions::satisfies(
+			new VersionParser(),
+			'doctrine/dbal',
+			'<3.6'
+		)) {
+			self::markTestSkipped('Test requires dbal 3 or 4.');
+		}
 		$this->analyse([__DIR__ . '/data/connection.php'], [
 			[
 				'Parameter at 0 is an array, but is not hinted as such to doctrine.',
