f42828ad684e026054c3d94161d89870150bc3da
Kamil Žáček <kamil.zacek@gmail.com>
2024-03-20 14:16:13 +0100

    Append Doctrine proxy class names to list of forbidden class names to disallow direct use of Doctrine proxy classes.

---
+++ tests/Classes/data/forbidden-class-name.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace App\GeneratedProxy\__CG__\App;
+
+class TestDoctrineEntity
+{
+}
+
+namespace _PHPStan_15755dag8c;
+
+class TestPhpStanEntity
+{
+}
+
+namespace ForbiddenNameClassExtension;
+
+use App\GeneratedProxy\__CG__\App\TestEntity;
+
+$doctrineEntity = new \App\GeneratedProxy\__CG__\App\TestDoctrineEntity();
+$phpStanEntity = new \_PHPStan_15755dag8c\TestPhpStanEntity();
