2114879962d3451b3aa22db20e0dcca5281d66b3
Kamil Žáček <kamil.zacek@gmail.com>
2024-03-20 14:16:13 +0100

    Append Doctrine proxy class names to list of forbidden class names to disallow direct use of Doctrine proxy classes.

---
+++ src/Classes/DoctrineProxyForbiddenClassNamesExtension.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Classes;
+
+use Doctrine\Persistence\Proxy;
+use PHPStan\Type\Doctrine\ObjectMetadataResolver;
+
+class DoctrineProxyForbiddenClassNamesExtension implements ForbiddenClassNameExtension
+{
+
+	/** @var ObjectMetadataResolver */
+	private $objectMetadataResolver;
+
+	public function __construct(ObjectMetadataResolver $objectMetadataResolver)
+	{
+		$this->objectMetadataResolver = $objectMetadataResolver;
+	}
+
+	public function getClassPrefixes(): array
+	{
+		$objectManager = $this->objectMetadataResolver->getObjectManager();
+		if ($objectManager === null) {
+			return [];
+		}
+
+		$entityManagerInterface = 'Doctrine\ORM\EntityManagerInterface';
+
+		if (!$objectManager instanceof $entityManagerInterface) {
+			return [];
+		}
+
+		return [
+			'Doctrine' => $objectManager->getConfiguration()->getProxyNamespace() . '\\' . Proxy::MARKER,
+		];
+	}
+
+}
