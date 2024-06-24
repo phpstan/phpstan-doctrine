d0b8d57980426e9d6061e605e8ed363cdf01c034
Jan Nedbal <janedbal@gmail.com>
2024-06-24 16:58:04 +0200

    Fix test in dbal4

--- tests/Platform/Entity/PlatformEntity.php
+++ tests/Platform/Entity/PlatformEntity.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace PHPStan\Platform\MatrixEntity;
+namespace PHPStan\Platform\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -8,49 +8,60 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="test")
  * @ORM\Entity
  */
-class TestEntity
+#[ORM\Table(name: 'test')]
+#[ORM\Entity]
+class PlatformEntity
 {
 
 	/**
 	 * @ORM\Id
+	 * @ORM\Column(type="string",nullable=false)
+	 * @var string
+	 */
+	#[ORM\Id]
+	#[ORM\Column(type: 'string', nullable: false)]
+	public $id;
+
+	/**
 	 * @ORM\Column(type="string", name="col_string", nullable=false)
 	 * @var string
 	 */
+	#[ORM\Column(type: 'string', name: 'col_string', nullable: false)]
 	public $col_string;
 
 	/**
-	 * @ORM\Id
 	 * @ORM\Column(type="boolean", name="col_bool", nullable=false)
 	 * @var bool
 	 */
+	#[ORM\Column(type: 'boolean', name: 'col_bool', nullable: false)]
 	public $col_bool;
 
 	/**
-	 * @ORM\Id
 	 * @ORM\Column(type="float", name="col_float", nullable=false)
 	 * @var float
 	 */
+	#[ORM\Column(type: 'float', name: 'col_float', nullable: false)]
 	public $col_float;
 
 	/**
-	 * @ORM\Id
 	 * @ORM\Column(type="decimal", name="col_decimal", nullable=false, scale=1, precision=2)
 	 * @var string
 	 */
+	#[ORM\Column(type: 'decimal', name: 'col_decimal', nullable: false, scale: 1, precision: 2)]
 	public $col_decimal;
 
 	/**
-	 * @ORM\Id
 	 * @ORM\Column(type="integer", name="col_int", nullable=false)
 	 * @var int
 	 */
+	#[ORM\Column(type: 'integer', name: 'col_int', nullable: false)]
 	public $col_int;
 
 	/**
-	 * @ORM\Id
 	 * @ORM\Column(type="bigint", name="col_bigint", nullable=false)
-	 * @var string
+	 * @var int|string
 	 */
+	#[ORM\Column(type: 'bigint', name: 'col_bigint', nullable: false)]
 	public $col_bigint;
 
 }
