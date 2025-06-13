ca4aeaf6b8e687a8beb2aefd7e5cbe4bbfc1cecc
Jan Nedbal <janedbal@gmail.com>
2025-06-13 17:39:58 +0200

    Direct call, but muted

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,8 @@ use PHPStan\Testing\PHPStanTestCase;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+@ini_set('assert.exception', '0'); // @ deprecated in PHP 8.3
+
 PHPStanTestCase::getContainer();
 
 require_once __DIR__ . '/orm-3-bootstrap.php';
